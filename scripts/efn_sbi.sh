b0d8dd1cfc566c448fb3606cf9ade7e4aec814fc
DepInfoAliciaSHAO <alicia.shao@depinfonancy.net>
2025-06-20 16:06:25 +0200

    4 GPUS

--- scripts/efn_sbi.sh
+++ scripts/efn_sbi.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-CUDA_VISIBLE_DEVICES=0 python scripts/train.py --cfg configs/efn4_fpn_sbi_adv.yaml
+CUDA_VISIBLE_DEVICES=0,1,2,3 python scripts/train.py --cfg configs/efn4_fpn_sbi_adv.yaml
